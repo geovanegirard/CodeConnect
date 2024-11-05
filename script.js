414be88b06405dfc84b3ae4976e4579e3d5879b0
Geovane Girard <g.ng2009@hotmail.com>
2024-11-05 20:07:50 -0300

    Implementando fileReader.

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const inputUpload = document.getElementById("imagem-upload");
 uploadBtn.addEventListener("click", () => {
     inputUpload.click();
 })
+
+function lerConteudoDoArquivo(arquivo) {
+    return new promise ((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({ url: leitor.result, nome: arquivo.nome})
+        }
+
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
+        }
+
+        leitor.readAsDataURL(arquivo)
+    })
+}
